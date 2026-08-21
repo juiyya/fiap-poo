56b75e09b6cacf9e7aaffd9cd058eec15d9977e7
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-20 21:02:54 -0300

    feat: adiciona métodos de comportamento e atualiza README

--- src/br/com/fiapride/main/SistemaPrincipal.java
+++ src/br/com/fiapride/main/SistemaPrincipal.java
@@ -1,14 +1,11 @@
 package br.com.fiapride.main;
 
-// Importamos a classe Passageiro para que o sistema a reconheça
-import br.com.fiapride.model.Pet;
+import br.com.fiapride.model.Passageiro;
 
 public class SistemaPrincipal {
 
     public static void main(String[] args) {
-        // INSTANCIAÇÃO
-        // O comando 'new' aloca memória para um novo objeto.
-        // Criando o primeiro passageiro (Objeto 1)
+
        //  Passageiro passageiro1 = new Passageiro();
         // passageiro1.nome = "Ana Silva";
         // passageiro1.saldo = 50.0;
@@ -23,14 +20,38 @@ public class SistemaPrincipal {
         // System.out.println("Passageiro: " + passageiro1.nome + " | Saldo: R$" + passageiro1.saldo);
         // System.out.println("Passageiro: " + passageiro2.nome + " | Saldo: R$" + passageiro2.saldo);
         
-    	Pet meuPet = new Pet();
-    	meuPet.nome = "Kita";
-    	meuPet.cor = "Preto";
+    	// DEAFIO 
+    	
+    	// Pet meuPet = new Pet();
+    	// meuPet.nome = "Kita";
+    	// meuPet.cor = "Preto";
     	// Fabriquei a segunda (Instância 2)
-    	Pet petDoProfessor = new Pet();
-    	petDoProfessor.nome = "Kato";
-    	petDoProfessor.cor = "Branco";
-    	System.out.println("Meu Pet é: " + meuPet.cor);
-    	System.out.println("O do Professor é: " + petDoProfessor.cor);
+    	// Pet petDoProfessor = new Pet();
+    	// petDoProfessor.nome = "Kato";
+    	// petDoProfessor.cor = "Branco";
+    	// System.out.println("Meu Pet é: " + meuPet.cor);
+    	// System.out.println("O do Professor é: " + petDoProfessor.cor);
+    	
+           
+            Passageiro passageiro1 = new Passageiro("Ana Silva", "222");
+            System.out.println("Regarga passageiro 1");
+            passageiro1.adicionarSaldo(50.0);
+
+            // Criando o segundo passageiro (Objeto 2)
+            Passageiro passageiro2 = new Passageiro("Carlos Souza", "333");
+            System.out.println("Regarga passageiro 2");
+            passageiro2.adicionarSaldo(12.5);
+
+            System.out.println("--- Sistema FiapRide ---");
+            System.out.println("Passageiro: " + passageiro1.nome + " | Saldo: R$ " + passageiro1.saldo + " | CPF: " + passageiro1.cpf);
+            System.out.println("Passageiro: " + passageiro2.nome + " | Saldo: R$ " + passageiro2.saldo + " | CPF: " + passageiro2.cpf);
+            
+            System.out.println("Pagando viagem do passageiro 1");
+            passageiro1.pagarViagem(20);
+            System.out.println("Pagando viagem do passageiro 2");
+            passageiro2.pagarViagem(20);
+
     }
 }
+    	
+    
